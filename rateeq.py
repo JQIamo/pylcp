99fe4b4ae61d9d1a1b78a4258c72a70fb0d765f2
Stephen Eckel <stephen.eckel@nist.gov>
2020-04-01 17:37:23 -0400

    Revert "rateeq.py: potential performance improvements"
    This reverts commit 21a3f5bee32c97aba5bcc57384c613e51f49127a.

--- rateeq.py
+++ rateeq.py
@@ -283,7 +283,8 @@ class rateeq():
         self.N0 = N0
 
     def set_initial_pop_from_equilibrium(self):
-        self.N0 = self.equilibrium_populations(self.r0, self.v0, t=0)
+        Rev, Rijl = self.construct_evolution_matrix(self.r0, self.v0, t=0)
+        self.N0 = self.equilibrium_populations(Rev)
 
 
     def evolve_populations(self, t_span, **kwargs):
