Update: Lot of minus sign errors fixed.
Have verified that under magnetic field evolution, spins spin in the 
correct direction.  Have also verified that laser fields pump in the 
correct direction.  Next step is to verify appropriate pumping under 
magnetic field shifts.

Need to redo force calculation to be more robust against changes in 
scalar dot product.

SPE

examples/basics/hamiltonians/02_hyperfine_hamiltonians.py
@@ -53,7 +53,7 @@ def breitrabi(B, gJ, gI, AHFS, J=1/2, I=3/2):
     return E, Es_max
 
 
-def diagnolize_and_sort(B, I, gI, state, method='uncoupled', 
+def diagnolize_and_sort(B, I, gI, state, method='uncoupled',
                         Bhat=np.array([0,0,1]), dosort=True):
     """
     Diagonalize and sort does what you would expect.
@@ -130,10 +130,10 @@ def diagnolize_and_sort(B, I, gI, state, method='uncoupled',
 # 7Li:
 B = np.arange(0.1, 1000, 2)
 
-Es, Vecs = diagnolize_and_sort(B, atom_Li7.I, atom_Li7.gI, 
+Es, Vecs = diagnolize_and_sort(B, atom_Li7.I, atom_Li7.gI,
                                atom_Li7.state[0], method='uncoupled')
 
-Es_2, Vecs_2 = diagnolize_and_sort(B, atom_Li7.I, atom_Li7.gI, 
+Es_2, Vecs_2 = diagnolize_and_sort(B, atom_Li7.I, atom_Li7.gI,
                                    atom_Li7.state[0], method='coupled')
 
 Es_3, Es_max = breitrabi(
@@ -154,10 +154,10 @@ ax.set_ylabel("$E/h$ (GHz)")
 # 6Li:
 B = np.linspace(0.1, 200, 101)
 
-Es, Vecs = diagnolize_and_sort(B, atom_Li6.I, atom_Li6.gI, 
+Es, Vecs = diagnolize_and_sort(B, atom_Li6.I, atom_Li6.gI,
                                atom_Li6.state[0], method='uncoupled')
 
-Es_2, Vecs_2 = diagnolize_and_sort(B, atom_Li6.I, atom_Li6.gI, 
+Es_2, Vecs_2 = diagnolize_and_sort(B, atom_Li6.I, atom_Li6.gI,
                                    atom_Li6.state[0], method='coupled')
 
 Es_3, Es_max = breitrabi(
@@ -178,10 +178,10 @@ ax.set_ylabel("$E/h$ (GHz)")
 nstates = int((2*atom_Rb87.state[2].J+1)*(2*atom_Rb87.I+1))
 B = np.linspace(0, 500, 101)
 
-Es, Vecs = diagnolize_and_sort(B, atom_Rb87.I, atom_Rb87.gI, 
+Es, Vecs = diagnolize_and_sort(B, atom_Rb87.I, atom_Rb87.gI,
                                atom_Rb87.state[2], method='uncoupled')
 
-Es_2, Vecs_2 = diagnolize_and_sort(B, atom_Rb87.I, atom_Rb87.gI, 
+Es_2, Vecs_2 = diagnolize_and_sort(B, atom_Rb87.I, atom_Rb87.gI,
                                    atom_Rb87.state[2], method='coupled')
 
 plt.figure("Rb-87 5P_{3/2} state", figsize=(3.25, 2.75))

hamiltonian.py
@@ -280,19 +280,16 @@ class hamiltonian():
 
 
     def return_full_H(self, Eq, Bq):
-        if isattr('H_0', self):
+        if not hasattr(self, 'H_0'):
             self.make_full_matrices()
 
-        H = np.zeros((self.n, self.n), dtype='complex128')
+        H = self.H_0 - np.tensordot(self.mu_q, np.conjugate(Bq), axes=(0, 0))
 
-        H += self.H_0 + np.tensordot(np.array([-1, 1, -1])*Bq[::-1],
-                                    self.mu_q, axes=(0, 0))
-
-        for key in self.d_q.keys():
-            H += 0.5*np.tensordot(np.conjugate(Eq[key]),
-                                  self.d_q_bare[key], axes=(0, 0)) -\
-                 0.5*np.tensordot(Eq[key],
-                                  self.d_q_star[key], axes=(0, 0))
+        for key in self.d_q_bare.keys():
+            H -= 0.5*np.tensordot(self.d_q_bare[key], np.conjugate(Eq[key]),
+                                  axes=(0, 0)) -\
+                 0.5*np.tensordot(self.d_q_star[key], Eq[key],
+                                  axes=(0, 0))
 
         return H
 
@@ -356,9 +353,9 @@ class hamiltonian():
                 # It isn't? Diagonalize it:
                 if not self.diagonal[ii]:
                     if isinstance(diag_block, tuple):
-                        H = (diag_block[0].matrix + B*diag_block[1].matrix[1])
+                        H = (diag_block[0].matrix - B*diag_block[1].matrix[1])
                     elif isinstance(diag_block, self.vector_block):
-                        H = B*diag_block.matrix[1]
+                        H = -B*diag_block.matrix[1]
                     else:
                         H = diag_block.matrix
 
@@ -380,10 +377,10 @@ class hamiltonian():
                 else: # It is diagonal:
                     if isinstance(diag_block, tuple):
                         self.rotated_hamiltonian.blocks[ii, ii].matrix = \
-                            diag_block[0].matrix + B*diag_block[1].matrix[1]
+                            diag_block[0].matrix - B*diag_block[1].matrix[1]
                     elif isinstance(diag_block, self.vector_block):
                         self.rotated_hamiltonian.blocks[ii, ii].matrix = \
-                            B*diag_block.matrix[1]
+                            -B*diag_block.matrix[1]
                     else:
                         self.rotated_hamiltonian.blocks[ii, ii].matrix = \
                             diag_block.matrix
@@ -408,10 +405,10 @@ class hamiltonian():
             for ii, diag_block in enumerate(np.diag(self.blocks)):
                 if isinstance(diag_block, tuple):
                     self.rotated_hamiltonian.blocks[ii, ii].matrix = \
-                        np.real(diag_block[0].matrix + B*diag_block[1].matrix[1])
+                        np.real(diag_block[0].matrix - B*diag_block[1].matrix[1])
                 elif isinstance(diag_block, self.vector_block):
                     self.rotated_hamiltonian.blocks[ii, ii].matrix = \
-                        np.real(B*diag_block.matrix[1])
+                        np.real(-B*diag_block.matrix[1])
                 else:
                     self.rotated_hamiltonian.blocks[ii, ii].matrix = \
                         np.real(diag_block.matrix)

hamiltonians/__init__.py
@@ -175,30 +175,31 @@ def hyperfine_coupled(J, I, gJ, gI, Ahfs, Bhfs=0, Chfs=0,
         H_0[ii,ii] = diag_elem[ii]
 
     # Now work on the field dependent part:
-    H_Bq = np.zeros((3, num_of_states, num_of_states))
+    mu_q = np.zeros((3, num_of_states, num_of_states))
 
     for ii, q in enumerate(range(-1, 2)):
         # TODO: Verify sign of q!
-        for F in np.arange(Fmin, Fmax+0.5, 1):
-            for Fp in np.arange(Fmin, Fmax+0.5, 1):
-                for mF in np.arange(-F, F+0.5, 1):
-                    if np.abs(mF+q)<=Fp:
-                        H_Bq[ii, index(F, mF), index(Fp, mF + q)] += gJ*muB*\
-                        (-1)**(F-mF)*wig3j(F, 1, Fp, mF, q, -(mF+q))*\
+        for Fp in np.arange(Fmin, Fmax+0.5, 1):
+            for F in np.arange(Fmin, Fmax+0.5, 1):
+                for mFp in np.arange(-Fp, Fp+0.5, 1):
+                    mF = mFp+q
+                    if np.abs(mF)<=Fp:
+                        mu_q[ii, index(F, mF), index(Fp, mFp)] -= gJ*muB*\
+                        (-1)**np.abs(F-mF)*wig3j(Fp, 1, F, -mF, q, mFp)*\
                         np.sqrt((2*Fp+1)*(2*F+1))*(-1)**(J+I+Fp+1)*\
                         wig6j(J, F, I, Fp, J, 1)*\
                         np.sqrt(J*(J+1)*(2*J+1))
 
-                        H_Bq[ii, index(F, mF), index(Fp, mF + q)] += gI*muB*\
-                        (-1)**(F-mF)*wig3j(F, 1, Fp, mF, q, -(mF+q))*\
+                        mu_q[ii, index(F, mF), index(Fp, mF - q)] += gI*muB*\
+                        (-1)**np.abs(F-mF)*wig3j(Fp, 1, F, -mF, q, mFp)*\
                         np.sqrt((2*Fp+1)*(2*F+1))*(-1)**(J+I+Fp+1)*\
                         wig6j(I, F, J, Fp, I, 1)*\
                         np.sqrt(I*(I+1)*(2*I+1))
 
     if return_basis:
-        return H_0, H_Bq, np.vstack((Fs, mFs))
+        return H_0, mu_q, np.vstack((Fs, mFs))
     else:
-        return H_0, H_Bq
+        return H_0, mu_q
 
 
 def singleF(F, gF=1, muB=(cts.value("Bohr magneton in Hz/T")*1e-4),
@@ -210,25 +211,30 @@ def singleF(F, gF=1, muB=(cts.value("Bohr magneton in Hz/T")*1e-4),
 
     # Initialize the matrix
     H_0 = np.zeros((int(2*F+1), int(2*F+1)))
-    H_Bq = np.zeros((3, int(2*F+1), int(2*F+1)))
+    mu_q = np.zeros((3, int(2*F+1), int(2*F+1)))
 
     # No diagonal elements
     # Off-diagonal elemnts:
     for ii, q in enumerate(np.arange(-1, 2, 1)):
-        for mF in np.arange(-F, F+1, 1):
-            if np.abs(mF+q) <= F:
-                H_Bq[ii, index(mF), index(mF + q)] += gF*muB*\
+        for mFp in np.arange(-F, F+1, 1):
+            mF = mFp + q
+            if np.abs(mF) <= F:
+                # The minus sign here comes from the fact that the hyperfine
+                # magnetic moment is dominated by the electron, whose magnetic
+                # moment points in the opposite direction as the spin.
+                mu_q[ii, index(mF), index(mFp)] -= gF*muB*\
                     (-1)**(F-mF)*np.sqrt(F*(F+1)*(2*F+1))*\
-                    wig3j(F, 1, F, mF, q, -(mF+q))
+                    wig3j(F, 1, F, -mF, q, mFp)
 
     if return_basis:
         basis = np.zeros((int(2*F+1), 2))
         basis[:, 0] = F
-        basis[:, 1] = np.arange(-F, F+1)
+        for mF in np.arange(-F, F+1, 1):
+            basis[index(mF), 1] = mF
 
-        argout = (H_0, H_Bq, basis)
+        argout = (H_0, mu_q, basis)
     else:
-        argout = (H_0, H_Bq)
+        argout = (H_0, mu_q)
 
     return argout
 
@@ -249,8 +255,8 @@ def dqij_two_hyperfine_manifolds(J1, J2, I, normalize=True, return_basis=False):
     transition.
     """
     def matrix_element(J, F, m_F, Jp, Fp, m_Fp, I, q):
-        return (-1)**(F-m_F+J+I+Fp+1)*np.sqrt((2*F+1)*(2*Fp+1))*\
-            wig3j(F, 1, Fp, m_F, q, -m_Fp)*wig6j(J, F, I, Fp, Jp, 1)
+        return (-1)**(Fp+1-m_F+J+I+Fp+1)*np.sqrt((2*F+1)*(2*Fp+1))*\
+            wig3j(Fp, 1, F, m_Fp, q, -m_F)*wig6j(J, F, I, Fp, Jp, 1)
 
     # A simple function for addressing the index:
     index = lambda Fmin, F, mF: coupled_index(F, mF, Fmin)
@@ -269,10 +275,10 @@ def dqij_two_hyperfine_manifolds(J1, J2, I, normalize=True, return_basis=False):
         for F1 in np.arange(F1min, F1max+0.5, 1):
             for F2 in np.arange(F2min, F2max+0.5, 1):
                 for mF in np.arange(-F1, F1+0.5, 1):
-                    if not np.abs(mF+q) > F2:
+                    if not np.abs(mF-q) > F2:
                         dqij[ii, index(F1min, F1, mF),
-                             index(F2min, F2, mF+q)] =\
-                        matrix_element(J1, F1, mF, J2, F2, mF+q, I, q)
+                             index(F2min, F2, mF-q)] =\
+                        matrix_element(J1, F1, mF, J2, F2, mF-q, I, q)
 
     if normalize:
         dqij = dqij_norm(dqij)
@@ -296,26 +302,37 @@ def dqij_two_hyperfine_manifolds(J1, J2, I, normalize=True, return_basis=False):
     return argout
 
 
-def dqij_two_bare_hyperfine(F1, F2, normalize=True):
+def dqij_two_bare_hyperfine(F, Fp, normalize=True):
     """
-    Calculates the dqij matrix for two bare hyperfine states:
-    """
-    # Make a simple function to return the matrix element:
-    def matrix_element(F, m_F, Fp, m_Fp, q):
-        return (-1)**(F-m_F)*wig3j(F, 1, Fp, m_F, q, -m_Fp)
+    Calculates the dqij matrix for two bare hyperfine states.  Specifically,
+    it returns the matrix of the operator $d_q$, where a photon is created by
+    a transition from the excited state to the ground state.
 
+    Parameters
+    ----------
+    F : integer or float (half integer)
+        Total angular momentum quantum number of the F state.
+    Fp : integer or float (half integer)
+        Total angular momentum quantum number of the F\' state.
+    normalize : boolean
+        By default, 'normalize' is True
+    """
     # A simple function for addressing the index:
     index = lambda F, mF: int(F+mF)
 
-    # Initialize the matrix:
-    dqij = np.zeros((3, int(2*F1+1), int(2*F2+1)))
+    # Initialize the matrix.  'Ground' state is represented by rows, 'excited'
+    # state by columns.
+    dqij = np.zeros((3, int(2*F+1), int(2*Fp+1)))
 
-    # Populate the matrix:
+    # Populate the matrix.  First go through each q:
     for ii, q in enumerate(np.arange(-1, 2, 1)):
-        for m_F1 in np.arange(-F1, F1+1, 1):
-            if not np.abs(m_F1+q) > F2:
-                dqij[ii, index(F1, m_F1), index(F2, m_F1 + q)] =\
-                    matrix_element(F1, m_F1, F2, m_F1 + q, q)
+        # Go through each m_F2:
+        for m_Fp in np.arange(-Fp, Fp+1, 1):
+            # m_F1 takes on a q value:
+            m_F = m_Fp+q
+            if not np.abs(m_F) > F:
+                dqij[ii, index(F, m_F), index(Fp, m_Fp)] =\
+                    (-1)**(F-m_F)*wig3j(F, 1, Fp, -m_F, q, m_Fp)
 
     # Normalization
     """

lasers.py
@@ -1,5 +1,6 @@
 import numpy as np
 from inspect import signature
+from .common import cart2spherical, spherical2cart
 
 import numba
 
@@ -98,19 +99,13 @@ class laserBeam():
                     if np.abs(np.dot(self.kvec, pol)) > 1e-9:
                         raise ValueError("I'm sorry; light is a transverse wave")
 
-                    # Always store in spherical basis:
-                    self.pol = np.array([(+pol[0]-1j*pol[1])/np.sqrt(2),
-                                         pol[2],
-                                         (-pol[0]-1j*pol[1])/np.sqrt(2)])
-
+                    # Always store in spherical basis.  Matches Brown and Carrington.
+                    self.pol = cart2spherical(pol).astype('complex128')
 
                 # The user has specified a single polarization vector in
                 # spherical basis:
                 elif pol_coord=='spherical':
-                    pol_cart = np.array([(-pol[2]+pol[0])/np.sqrt(2),
-                                         1j*(pol[2]+pol[0])/np.sqrt(2),
-                                         pol[1]
-                                         ])
+                    pol_cart = spherical2cart(pol)
 
                     # Check for transverseness:
                     if np.abs(np.dot(self.kvec, pol_cart)) > 1e-9:
@@ -335,10 +330,7 @@ class laserBeam():
         Returns the polarization in Cartesian coordinates.
         """
         pol = self.return_pol(R, t)
-        return np.array([(-pol[2]+pol[0])/np.sqrt(2),
-                         1j*(pol[2]+pol[0])/np.sqrt(2),
-                         pol[1]
-                        ])
+        return spherical2cart(pol)
 
     def jones_vector(self, xp, yp, R=np.array([0., 0., 0.]), t=0):
         """
@@ -449,13 +441,13 @@ class laserBeam():
         amp = np.sqrt(beta/2)
 
         if isinstance(t, float) or (isinstance(t, np.ndarray) and t.size==1):
-            Eq = pol*amp*np.exp(-1j*np.dot(kvec, R) + 1j*delta*t +
-                                -1j*self.phase)
+            Eq = pol*amp*np.exp(1j*np.dot(kvec, R) - 1j*delta*t +
+                                1j*self.phase)
         else:
             Eq = np.multiply(
                 pol.reshape(3, t.size),
-                amp*np.exp(-1j*dot2D(kvec, R) + 1j*delta*t +
-                           -1j*self.phase)
+                amp*np.exp(1j*dot2D(kvec, R) - 1j*delta*t +
+                           1j*self.phase)
             )
 
         return Eq
@@ -472,14 +464,14 @@ class laserBeam():
 
         if isinstance(t, float) or (isinstance(t, np.ndarray) and t.size==1):
             delE = np.multiply(kvec.reshape(3, 1), pol.reshape(1, 3))*\
-                 1j*amp*np.exp(-1j*np.dot(kvec, R) + 1j*delta*t +
+                 1j*amp*np.exp(1j*np.dot(kvec, R) - 1j*delta*t +
                                           -1j*self.phase)
         else:
             delE = np.multiply(
                 np.multiply(kvec.reshape(3, 1, t.size),
                             pol.reshape(1, 3, t.size)),
-                1j*amp*np.exp(-1j*dot2D(kvec, R) + 1j*delta*t +
-                              -1j*self.phase)
+                1j*amp*np.exp(1j*dot2D(kvec, R) - 1j*delta*t +
+                              1j*self.phase)
             )
 
         return delE

obe.py
@@ -11,6 +11,7 @@ from scipy.integrate import solve_ivp
 from .rateeq import rateeq
 from .lasers import laserBeams
 from .common import printProgressBar
+from .common import printProgressBar, spherical_dot, cart2spherical, spherical2cart
 
 @numba.vectorize([numba.float64(numba.complex128),numba.float32(numba.complex64)])
 def abs2(x):
@@ -188,20 +189,20 @@ class obe():
             )
         self.ev_mat['B'] = np.array(self.ev_mat['B'])
 
-        self.ev_mat['E'] = {}
-        self.ev_mat['E*'] = {}
+        self.ev_mat['d_q'] = {}
+        self.ev_mat['d_q*'] = {}
         for key in self.laserBeams.keys():
-            self.ev_mat['E'][key] = [None]*3
-            self.ev_mat['E*'][key] = [None]*3
+            self.ev_mat['d_q'][key] = [None]*3
+            self.ev_mat['d_q*'][key] = [None]*3
             for q in range(3):
-                self.ev_mat['E'][key][q] = self.build_coherent_ev_submatrix(
+                self.ev_mat['d_q'][key][q] = self.build_coherent_ev_submatrix(
                     self.hamiltonian.d_q_bare[key][q]
                 )
-                self.ev_mat['E*'][key][q] = self.build_coherent_ev_submatrix(
+                self.ev_mat['d_q*'][key][q] = self.build_coherent_ev_submatrix(
                     self.hamiltonian.d_q_star[key][q]
                 )
-            self.ev_mat['E'][key] = np.array(self.ev_mat['E'][key])
-            self.ev_mat['E*'][key] = np.array(self.ev_mat['E*'][key])
+            self.ev_mat['d_q'][key] = np.array(self.ev_mat['d_q'][key])
+            self.ev_mat['d_q*'][key] = np.array(self.ev_mat['d_q*'][key])
 
 
     def build_decay_ev(self):
@@ -329,30 +330,23 @@ class obe():
 
         self.ev_mat['reE'] = {}
         self.ev_mat['imE'] = {}
-        for key in self.ev_mat['E'].keys():
+        for key in self.ev_mat['d_q'].keys():
             self.ev_mat['reE'][key] = np.array([self.transform_ev_matrix(
-                self.ev_mat['E'][key][jj] + self.ev_mat['E*'][key][jj]
+                self.ev_mat['d_q'][key][jj] + self.ev_mat['d_q*'][key][jj]
                 ) for jj in range(3)])
             self.ev_mat['imE'][key] = np.array([self.transform_ev_matrix(
-                -1j*(self.ev_mat['E'][key][jj] - self.ev_mat['E*'][key][jj])
+                -1j*(self.ev_mat['d_q'][key][jj] - self.ev_mat['d_q*'][key][jj])
                 ) for jj in range(3)])
 
         # Transform Bq back into Bx, By, and Bz (making it real):
-        ev_mat_Bx = self.transform_ev_matrix(
-            1/np.sqrt(2)*(self.ev_mat['B'][0] - self.ev_mat['B'][2])
-            )
-        ev_mat_By = self.transform_ev_matrix(
-            1j/np.sqrt(2)*(self.ev_mat['B'][0] + self.ev_mat['B'][2])
-            )
-        ev_mat_Bz = self.transform_ev_matrix(
-            self.ev_mat['B'][1]
-            )
+        self.ev_mat['B'] = spherical2cart(self.ev_mat['B'][0])
 
-        del self.ev_mat['B']
-        self.ev_mat['B'] = np.array([ev_mat_Bx, ev_mat_By, ev_mat_Bz])
+        for jj in range(3):
+            self.ev_mat['B'][jj] = self.transform_ev_matrix(self.ev_mat['B'][jj])
 
-        del self.ev_mat['E']
-        del self.ev_mat['E*']
+        del self.ev_mat['d_q']
+        del self.ev_mat['d_q*']
+
 
     def convert_to_sparse(self):
         def convert_based_on_shape(matrix):
@@ -396,11 +390,7 @@ class obe():
         if self.transform_into_re_im:
             return B
         else:
-            Bq = np.zeros((3,), dtype='complex128')
-
-            Bq[0] = B[0]/np.sqrt(2)+1j*B[1]/np.sqrt(2)
-            Bq[1] = B[2]
-            Bq[2] = -B[0]/np.sqrt(2)+1j*B[1]/np.sqrt(2)
+            Bq = cart2spherical(B)
 
             return Bq
 
@@ -491,17 +481,16 @@ class obe():
                         ev_mat -= 0.5*np.real(Eq[ii])*self.ev_mat['reE'][key][ii]
                         ev_mat -= 0.5*np.imag(Eq[ii])*self.ev_mat['imE'][key][ii]
             else:
-                Eq = self.laserBeams[key].total_electric_field(t, np.real(r))
+                Eq = self.laserBeams[key].total_electric_field(np.real(r), t)
                 for ii in range(3):
                     if np.abs(Eq[ii])>1e-10:
-                        ev_mat -= 0.5*np.conjugate(Eq[ii])*self.ev_mat['E'][key][ii]
-                        ev_mat -= 0.5*Eq[ii]*self.ev_mat['E*'][key][ii]
+                        ev_mat -= 0.5*np.conjugate(Eq[ii])*self.ev_mat['d_q'][key][ii]
+                        ev_mat -= 0.5*Eq[ii]*self.ev_mat['d_q*'][key][ii]
 
         # Add in magnetic fields:
         Bq = self.return_magnetic_field(r, t)
         for ii in range(3):
-            if np.abs(Bq[ii])>1e-10:
-                ev_mat += Bq[ii]*self.ev_mat['B'][ii]
+            ev_mat -= spherical_dot(self.ev_mat['B'], Bq)
 
         return ev_mat
 
@@ -520,21 +509,21 @@ class obe():
             if self.transform_into_re_im:
                 Eq = self.laserBeams[key].total_electric_field(r, t)
                 for ii in range(3):
-                    if np.abs(Eq[ii])>1e-10:
-                        drhodt -= 0.5*np.real(Eq[ii])*(self.ev_mat['reE'][key][ii] @ rho)
-                        drhodt -= 0.5*np.imag(Eq[ii])*(self.ev_mat['imE'][key][ii] @ rho)
+                    drhodt -= 0.5*np.real(Eq[ii])*(self.ev_mat['reE'][key][ii] @ rho)
+                    drhodt -= 0.5*np.imag(Eq[ii])*(self.ev_mat['imE'][key][ii] @ rho)
             else:
                 Eq = self.laserBeams[key].total_electric_field(np.real(r), t)
-                for ii in range(3):
-                    if np.abs(Eq[ii])>1e-10:
-                        drhodt -= 0.5*np.conjugate(Eq[ii])*(self.ev_mat['E'][key][ii] @ rho)
-                        drhodt -= 0.5*Eq[ii]*(self.ev_mat['E*'][key][ii] @ rho)
+                for ii, q in enumerate(np.arange(-1., 2., 1)):
+                    drhodt -= 0.5*(-1.)**q*Eq[2-ii]*(self.ev_mat['d_q'][key][ii] @ rho)
+                    drhodt -= 0.5*(-1.)**q*np.conjugate(Eq[2-ii])*(self.ev_mat['d_q*'][key][ii] @ rho)
 
         # Add in magnetic fields:
         Bq = self.return_magnetic_field(t, r)
-        for ii in range(3):
-            if np.abs(Bq[ii])>1e-10:
-                drhodt += Bq[ii]*(self.ev_mat['B'][ii] @ rho)
+        for ii, q in enumerate(range(-1, 2)):
+            if self.transform_into_re_im:
+                drhodt -= self.ev_mat['B'][ii]*Bq[ii] @ rho
+            else:
+                drhodt -= (-1)**np.abs(q)*self.ev_mat['B'][ii]*Bq[2-ii] @ rho
 
         return drhodt
 
@@ -808,19 +797,23 @@ class obe():
         """
         Reshape the solution to have all the proper parts.
         """
-        # This should just do it:
-        rho = self.sol.y[:-6].reshape(self.hamiltonian.n, self.hamiltonian.n,
-                                      self.sol.t.size)
+        rho = self.sol.y[:-6].astype('complex128')
 
-        # If not:
+        if self.transform_into_re_im:
+            for jj in range(rho.shape[1]):
+                rho[:, jj] = self.U @ rho[:, jj]
+
+        rho = rho.reshape(self.hamiltonian.n, self.hamiltonian.n,
+                          self.sol.t.size)
+        """# If not:
         if self.transform_into_re_im:
             new_rho = np.zeros(rho.shape, dtype='complex128')
             for jj in range(new_rho.shape[2]):
                 new_rho[:, :, jj] = (np.diag(np.diagonal(rho[:, :, jj])) +
                                      np.triu(rho[:, :, jj], k=1) +
-                                     np.triu(rho[:, :, jj], k=1).T -
-                                     1j*np.tril(rho[:, :, jj], k=-1) +
+                                     np.triu(rho[:, :, jj], k=1).T +
+                                     1j*np.tril(rho[:, :, jj], k=-1) -
                                      1j*np.tril(rho[:, :, jj], k=-1).T)
-            rho = new_rho
+            rho = new_rho"""
 
         return (self.sol.t, rho)

rateeq.py
@@ -47,7 +47,7 @@ class force_profile():
         for key in Rijl:
             self.Rijl[key][ind] = Rijl[key]
 
-        for key in f:
+        for key in f_laser:
             for jj in range(3):
                 self.f[key][(jj,) + ind] = f[key][jj]
 
@@ -220,7 +220,7 @@ class rateeq():
                             (1 + 4*(beam.delta - (H0[ng+jj, ng+jj] - H0[ii, ii]) -
                                     np.dot(kvec, v))**2)"""
                             self.Rijl[key][ll, ii, jj] = beta/2*\
-                                fijq/(1 + 4*((E2[jj] - E1[ii]) + delta -
+                                fijq/(1 + 4*(-(E2[jj] - E1[ii]) + delta -
                                              np.dot(kvec, v))**2)
 
             # Now add the pumping rates into the rate equation propogation matrix: