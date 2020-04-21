930959f8e8e8c1ecf4f121adbda365d9fa1f889d
Stephen Eckel <stephen.eckel@nist.gov>
2020-04-21 17:01:42 -0400

    Example update
    Added in the simple example of the a spin in an impossible gradient (one that just goes along a single axis).  But it is informative because we can solve this exactly.
    
    SPE

--- examples/dynamics/magnetic_traps/01_motion_OBE.py
+++ examples/dynamics/magnetic_traps/01_motion_OBE.py
@@ -20,13 +20,31 @@ hamiltonian = pylcp.hamiltonian()
 hamiltonian.add_H_0_block('g', H0)
 hamiltonian.add_mu_q_block('g', muq)
 
-magField = pylcp.magField(lambda R: np.array([-0.5*R[0], -0.5*R[1], 1*R[2]]))
+magField = pylcp.magField(lambda R: np.array([0., 0., R[2]]))
 laserBeams = {}
+
+obe = pylcp.obe(laserBeams, magField, hamiltonian, include_mag_forces=True)
+obe.set_initial_position(np.array([0., 0., 0.]))
+theta = 3*np.pi/4
+psi = np.array([np.cos(theta/2), np.sin(theta/2)])
+rho = np.array([[psi[0]*psi[0], psi[0]*psi[1]], [psi[1]*psi[0], psi[1]*psi[1]]])
+
+obe.set_initial_rho(rho.reshape(4,))
+obe.evolve_motion([0., 5.], )
+
+t, rho = obe.reshape_sol()
+fig, ax = plt.subplots(1, 2, figsize=(6.25, 2.75))
+[ax[0].plot(t, rho[ii, ii]) for ii in range(2)]
+ax[0].plot(t, np.real(rho[0, 1]), '--')
+ax[0].plot(t, np.imag(rho[0, 1]), '--')
+ax[1].plot(t, obe.sol.y[-3:, :].T)
 
 # %%
 """
-Now, let's try it with the OBE:
+Now, let's try the quadrupole trap with
 """
+magField = pylcp.magField(lambda R: np.array([-0.5*R[0], -0.5*R[1], 1*R[2]]))
+
 obe = pylcp.obe(laserBeams, magField, hamiltonian, include_mag_forces=True)
 
 obe.set_initial_position(5*(2*np.random.rand(3)-1))
