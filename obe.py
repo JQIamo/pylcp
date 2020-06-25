c2429f9a78bc1d3f6eee0340367f11fcebb8e278
Stephen Eckel <stephen.eckel@nist.gov>
2020-06-24 21:05:13 -0400

    obe.py: find_equilibrium_force(): fix a bug if maxiter is reached
    SPE

--- obe.py
+++ obe.py
@@ -844,7 +844,7 @@ class obe():
             self.piecewise_sols = []
 
         ii=0
-        while ii<itermax:
+        while True:
             if not Npts is None:
                 kwargs['t_eval'] = np.linspace(ii*deltat, (ii+1)*deltat, int(Npts))
 
@@ -861,6 +861,8 @@ class obe():
             if (np.sum((f_avg)**2)<abs or
                 np.sum((old_f_avg-f_avg)**2)/np.sum((f_avg)**2)<rel or
                 np.sum((old_f_avg-f_avg)**2)<abs):
+                break;
+            elif ii>=itermax-1:
                 break;
             else:
                 old_f_avg = copy.copy(f_avg)
