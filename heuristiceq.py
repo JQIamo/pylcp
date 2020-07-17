heuristic_eq: fixing factor of 2 bug in random recoil()
Turns out that it needds two random recoils for each scattering event (since the force is independently calculated).  DUH.

SPE

heuristiceq.py
@@ -145,7 +145,8 @@ class heuristiceq(object):
             num_of_scatters = 0
             total_P = np.sum(self.R)*dt
             if np.random.rand(1)<total_P:
-                y[0:3] += self.k/self.mass*random_vector()
+                y[0:3] += self.k/self.mass*(random_vector(free_axes)+
+                                            random_vector(free_axes))
                 num_of_scatters += 1
 
             new_dt_max = (max_scatter_probability/total_P)*dt